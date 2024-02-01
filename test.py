897b8987d92cd3a7307a6317e1c8b3aecff861c3
bloath86 <158464573+bloath86@users.noreply.github.com>
2024-02-01 22:09:26 +0900

    openpyxl 사용하지 않도록 수정된 코드

--- test.py
+++ test.py
@@ -7,7 +7,10 @@ st.title('MY STORE APP')
 file_path = 'test-rowdata.xlsx'
 
 # Excel 파일을 읽어와서 데이터프레임으로 변환
-df = pd.read_excel(file_path)
+excel_file = pd.ExcelFile(file_path)
+
+# 특정 시트를 선택하여 데이터프레임으로 변환
+df = pd.read_excel(excel_file, sheet_name='Sheet1')
 
 # 변수로 받은 상품 ID 값
 target_product_id = st.text_input("상품 ID를 입력하세요")
@@ -18,7 +21,5 @@ df['상품ID'] = df['상품ID'].astype(str)
 # 변수로 받은 값과 상품ID 값이 일치하는 데이터만 필터링
 filtered_df = df[df['상품ID'] == target_product_id]
 
-
 st.dataframe(df)
-
 st.dataframe(filtered_df)
